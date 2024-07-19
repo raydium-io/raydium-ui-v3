2fee58e8373865a1fafde6fb9a581f3c8b1f36da
bradley <bradley@reactorlabs.io>
2024-07-19 20:06:23 +0800

    fix:moonpay mobile style

--- src/features/Moonpay/Moonpay.tsx
+++ src/features/Moonpay/Moonpay.tsx
@@ -21,7 +21,7 @@ export default function MoonpayPage() {
   const { isMobile } = useResponsive()
   const phantomWallet = new PhantomWalletAdapter()
   const isPhantomInstalled = useMemo(() => phantomWallet.readyState !== WalletReadyState.NotDetected, [phantomWallet.readyState])
-  const [isNotTooTall] = useMediaQuery('(max-height: 1100px)')
+  const [isShortScreen, isMediumScreen] = useMediaQuery(['(max-height: 700px)', '(max-height: 1100px)'])
   const css = `
     .beta_tooltip, .navbar, .mobile_bottom_navbar {
       z-index: 1
@@ -57,121 +57,124 @@ export default function MoonpayPage() {
         overflow="auto"
         position="relative"
       >
-        <AppNavLayout>{null}</AppNavLayout>
-        <Flex
-          width="100%"
-          height="100%"
-          alignItems="center"
-          justifyContent="center"
-          background="url(/images/moonpay-dots.svg)"
-          backgroundPosition="center bottom 125px"
-          backgroundRepeat="no-repeat"
-          position="absolute"
-          top="0"
-          left="0"
-          {...(isMobile ? {} : { minHeight: '700px' })}
-        >
-          <Box
-            textAlign="center"
-            {...(isMobile ? { display: 'flex', flexDirection: 'column', px: '6' } : {})}
-            mb={isNotTooTall && !isMobile ? '25vh' : '0'}
+        <AppNavLayout>
+          <Flex
+            width="100%"
+            alignItems="center"
+            justifyContent="center"
+            background="url(/images/moonpay-dots.svg)"
+            backgroundPosition="center bottom 125px"
+            backgroundRepeat="no-repeat"
+            {...(isMobile
+              ? { height: isShortScreen ? 'auto' : '100%' }
+              : { position: 'absolute', top: '0', left: '0', height: '100%', minHeight: '700px' })}
           >
-            <Flex alignItems="center" justifyContent="center" gap={4} flexDirection="row">
-              <Flex
-                w={{ base: '64px', md: '104px' }}
-                h={{ base: '64px', md: '104px' }}
-                minWidth={{ base: '64px', md: '104px' }}
-                p={{ base: '10px', md: '16px' }}
-                borderRadius={{ base: '15px', md: '24px' }}
-                bgColor={colors.background02}
-                alignItems="center"
-                justifyContent="center"
-              >
-                <RaydiumLogo width="64" height="64" />
+            <Box
+              textAlign="center"
+              {...(isMobile ? { display: 'flex', flexDirection: 'column', px: '6' } : {})}
+              mb={isMediumScreen && !isMobile ? '25vh' : '0'}
+            >
+              <Flex alignItems="center" justifyContent="center" gap={4} flexDirection="row">
+                <Flex
+                  w={{ base: '64px', md: '104px' }}
+                  h={{ base: '64px', md: '104px' }}
+                  minWidth={{ base: '64px', md: '104px' }}
+                  p={{ base: '10px', md: '16px' }}
+                  borderRadius={{ base: '15px', md: '24px' }}
+                  bgColor={colors.background02}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <RaydiumLogo width="64" height="64" />
+                </Flex>
+                <Plus width="25px" height="25px" color="#8d93b7" />
+                <Flex
+                  w={{ base: '64px', md: '104px' }}
+                  h={{ base: '64px', md: '104px' }}
+                  minWidth={{ base: '64px', md: '104px' }}
+                  p={{ base: '10px', md: '16px' }}
+                  borderRadius={{ base: '15px', md: '24px' }}
+                  bgColor={colors.background02}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <MoonPayIcon width="100%" height="100%" color="#7715f5" />
+                </Flex>
               </Flex>
-              <Plus width="25px" height="25px" color="#8d93b7" />
-              <Flex
-                w={{ base: '64px', md: '104px' }}
-                h={{ base: '64px', md: '104px' }}
-                minWidth={{ base: '64px', md: '104px' }}
-                p={{ base: '10px', md: '16px' }}
-                borderRadius={{ base: '15px', md: '24px' }}
-                bgColor={colors.background02}
-                alignItems="center"
-                justifyContent="center"
-              >
-                <MoonPayIcon width="100%" height="100%" color="#7715f5" />
-              </Flex>
-            </Flex>
-            <Text as="h1" fontFamily="chillax" fontSize={['24px', '40px', '40px']} color={colors.text02} mt={6} mb={8}>
-              {t('moonpay.deposit_using')}
-              <Text as="span" fontWeight="semibold">
-                {t('moonpay.title')}
+              <Text as="h1" fontFamily="chillax" fontSize={['24px', '40px', '40px']} color={colors.text02} mt={6} mb={8}>
+                {t('moonpay.deposit_using')}
+                <Text as="span" fontWeight="semibold">
+                  {t('moonpay.title')}
+                </Text>
               </Text>
-            </Text>
-            <Box {...(isMobile ? { mt: '8', order: 2 } : {})}>
-              {isPhantomInstalled ? <WalletOnramp /> : <Text fontSize="sm">{t('moonpay.phantom_wallet_not_installed')}</Text>}
+              <Box {...(isMobile ? { mb: '4', mt: '4', order: 2 } : {})}>
+                {isPhantomInstalled ? <WalletOnramp /> : <Text fontSize="sm">{t('moonpay.phantom_wallet_not_installed')}</Text>}
+              </Box>
+              <Flex
+                maxW="728px"
+                m="0 auto"
+                borderTop={`1px solid ${colors.dividerBg}`}
+                pt={6}
+                color={colors.text03}
+                textAlign="left"
+                {...(isMobile
+                  ? {
+                      position: 'static',
+                      width: '100%',
+                      order: '1',
+                      mt: '3vh',
+                      gap: '4',
+                      direction: 'column'
+                    }
+                  : {
+                      justifyContent: 'space-between',
+                      gap: '20',
+                      mb: '5.5rem',
+                      direction: 'row',
+                      position: 'absolute',
+                      left: '50%',
+                      bottom: '10vh',
+                      transform: 'translateX(-50%)'
+                    })}
+              >
+                {['step1', 'step2', 'step3'].map((step, index) => (
+                  <Box key={index} flex="1" {...(isMobile ? { display: 'flex', gap: '4' } : {})}>
+                    <Box mb={isMobile ? 0 : '2'}>
+                      {index === 0 && <Phantom width="24" height="24" color="#898eff" />}
+                      {index === 1 && <SolGrey width="24" height="24" color="#898eff" />}
+                      {index === 2 && <Cart width="24" height="24" color="#898eff" />}
+                    </Box>
+                    <Box>
+                      <Text
+                        fontSize={{ base: 'sm', md: 'md' }}
+                        color={colors.text02}
+                        fontFamily="chillax"
+                        fontWeight="semibold"
+                        textTransform="uppercase"
+                        mb={1}
+                      >
+                        {t(`moonpay.${step}`)}
+                      </Text>
+                      <Text fontSize="sm">{t(`moonpay.${step}_text`)}</Text>
+                    </Box>
+                  </Box>
+                ))}
+              </Flex>
+              <Flex
+                width="100%"
+                justifyContent="center"
+                alignItems="center"
+                gap="3"
+                mt={8}
+                {...(isMobile ? {} : { position: 'absolute', bottom: '14', left: '0' })}
+              >
+                {['apple', 'google', 'mastercard', 'visa', 'paypal'].map((name, index) => (
+                  <Image key={index} src={`/images/payments/${name}.webp`} width="50px" height="auto" />
+                ))}
+              </Flex>
             </Box>
-            <Flex
-              maxW="728px"
-              m="0 auto"
-              borderTop={`1px solid ${colors.dividerBg}`}
-              pt={6}
-              left="50%"
-              bottom="10vh"
-              mb="88px"
-              color={colors.text03}
-              textAlign="left"
-              {...(isMobile
-                ? {
-                    position: 'static',
-                    width: '100%',
-                    marginBottom: '0',
-                    transform: 'none',
-                    order: '1',
-                    marginTop: '3vh',
-                    gap: '4',
-                    direction: 'column'
-                  }
-                : { justifyContent: 'space-between', gap: '80px', direction: 'row', position: 'absolute', transform: 'translateX(-50%)' })}
-            >
-              {['step1', 'step2', 'step3'].map((step, index) => (
-                <Box key={index} flex="1" {...(isMobile ? { display: 'flex', gap: '12px' } : {})}>
-                  <Box width="24px" height="24px">
-                    {index === 0 && <Phantom width="24" height="24" color="#898eff" />}
-                    {index === 1 && <SolGrey width="24" height="24" color="#898eff" />}
-                    {index === 2 && <Cart width="24" height="24" color="#898eff" />}
-                  </Box>
-                  <Box>
-                    <Text
-                      fontSize={{ base: 'sm', md: 'md' }}
-                      color={colors.text02}
-                      fontFamily="chillax"
-                      fontWeight="semibold"
-                      textTransform="uppercase"
-                      {...(isMobile ? {} : { mt: '8px', mb: '4px' })}
-                    >
-                      {t(`moonpay.${step}`)}
-                    </Text>
-                    <Text fontSize="sm">{t(`moonpay.${step}_text`)}</Text>
-                  </Box>
-                </Box>
-              ))}
-            </Flex>
-            <Flex
-              width="100%"
-              justifyContent="center"
-              alignItems="center"
-              gap="3"
-              mt={8}
-              {...(isMobile ? {} : { m: '0', position: 'absolute', bottom: '56px', left: '0' })}
-            >
-              {['apple', 'google', 'mastercard', 'visa', 'paypal'].map((name, index) => (
-                <Image key={index} src={`/images/payments/${name}.webp`} width="50px" />
-              ))}
-            </Flex>
-          </Box>
-        </Flex>
+          </Flex>
+        </AppNavLayout>
       </Box>
     </Box>
   )
