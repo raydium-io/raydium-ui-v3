e707a7730fd52f04e7c5276d8a908774e71d7c00
Cruz Shia <iamahan0720@gmail.com>
2025-02-20 18:13:15 +0800

    chore: specify toApr util

--- src/utils/numberish/toApr.ts
+++ src/utils/numberish/toApr.ts
@@ -2,6 +2,12 @@ import Decimal from 'decimal.js'
 import { getFirstNonZeroDecimal, formatLocaleStr } from './formatter'
 
 export default function toApr({ val, multiply = true, decimal }: { val: string | number; multiply?: boolean; decimal?: number }) {
-  const calVal = multiply ? new Decimal(val).mul(100).toString() : String(val ?? 0)
-  return formatLocaleStr(calVal, decimal === undefined ? Math.max(2, getFirstNonZeroDecimal(calVal) + 1) : decimal) + '%'
+  const decimalVal = new Decimal(val || 0)
+  const calVal = multiply ? decimalVal.mul(100).toString() : String(val ?? 0)
+  return (
+    formatLocaleStr(
+      calVal,
+      decimal === undefined ? (decimalVal.gte(0.01) ? 2 : Math.max(2, getFirstNonZeroDecimal(calVal) + 1)) : decimal
+    ) + '%'
+  )
 }
